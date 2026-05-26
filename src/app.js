1727e8e7c93a07a6aff520f03853434f4e8af682
root <root@DESKTOP-OFN301J>
2026-05-25 23:07:03 -0300

    Ajuste app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import express from "express"
+import express from "express";
 import router from "./routes/index.js";
 import { logger } from "./utils/logger.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import 'dotenv/config.js';
-app.use(cookieParser());
 
+// 1. PRIMEIRO: Você inicializa a variável 'app'
 const app = express();
 
+// 2. DEPOIS: Você usa o 'app' para registrar os middlewares
+app.use(cookieParser());
 app.use(express.json());
 app.use(logger);
 app.use(router);
